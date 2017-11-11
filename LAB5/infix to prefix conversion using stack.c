62c21fc38f6c4e3c50a6a2a4aceeca9997c753f7
Hardik <hardikrana276@gmail.com>
2017-11-11 09:44:18 +0530

    Update infix to prefix conversion using stack.c

--- LAB5/infix to prefix conversion using stack.c
+++ LAB5/infix to prefix conversion using stack.c
@@ -1,3 +1,9 @@
+/*
+   HARDIK RANA
+   
+   INFIX TO PREFIX CONVERSION-APPLICATIONS OF STACK
+*/
+
 #include<stdio.h>
 #include<string.h>
 #define size 100
@@ -47,7 +53,8 @@ int main()
       prefix[j]=temp;
       j++;
       temp=pop();
-     }}
+     }
+   }
   }
   while(top>-1)
   {
@@ -55,10 +62,13 @@ int main()
     j++;
   }
   prefix[j]='\0';
-  puts(strrev(prefix));
-  return 0;}
+  printf("the prefix expression is\n");
+  printf("%s",strrev(prefix));
+  return 0;
+}
+
 void push(char item)
-  {
+ {
     if(top>=size-1)
     printf("stack overfloaw\n");
     else
@@ -66,23 +76,26 @@ void push(char item)
      top++;
      stack[top]=item;
     }
-  }
-  int is_operator(char symbol)
-  {
+}
+
+int is_operator(char symbol)
+{
     if(symbol=='^'||symbol=='+'||symbol=='-'||symbol=='*'||symbol=='/')
         return 1;
     else
         return 0;
-  }
-  char pop()
-  {
+}
+ 
+char pop()
+{
     char item;
     item=stack[top];
     top--;
     return item;
-  }
-  int precendence(char symbol)
-  {
+}
+  
+int precendence(char symbol)
+{
     if(symbol=='^')
        return 3;
     else if(symbol=='/'||symbol=='*')
@@ -91,4 +104,4 @@ void push(char item)
         return 1;
     else
         return 0;
-  }
+ }
