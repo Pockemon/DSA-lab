8c4c73d425ced4f6cd2cacba95455f5004a53d63
Hardik <hardikrana276@gmail.com>
2017-11-11 07:57:42 +0530

    Update implementation of stack using linked list.c

--- LAB5/implementation of stack using linked list.c
+++ LAB5/implementation of stack using linked list.c
@@ -1,54 +1,68 @@
+/* HARDIK RANA
+
+   C PROGRAM TO IMPLEMENT STACK USING LINKED LIST
+
+*/
+
 #include<stdio.h>
 #include<stdlib.h>
+
 struct Node
 {
  int data;
  struct Node* next;
 };
 struct Node *top=NULL;
+
 void push(int num);
 int pop();
 void display();
 int main()
 {
- int choice,num;
- while(1)
- {
-  printf("\n 1 push\n");
-  printf("2 pop\n");
-  printf("3 display\n");
-  printf("4 exit\n");
-  scanf("%d",&choice);
-  switch(choice)
-  {
-   case 1:
-       printf("enter number to be inserted\n");
-       scanf("%d",&num);
-       push(num);
-       break;
-   case 2:
-       num=pop();
-       break;
-   case 3:
-       display();
-       break;
-   case 4:
-       exit(1);
-   default:
-       printf("invalid input\n");
-  }}}
+   int choice,num;
+   while(1)
+   {
+    printf("1 push\n");
+    printf("2 pop\n");
+    printf("3 display\n");
+    printf("4 exit\n");
+    scanf("%d",&choice);
+    switch(choice)
+    {
+     case 1:
+         printf("enter number to be inserted\n");
+         scanf("%d",&num);
+         push(num);
+         break;
+     case 2:
+        num=pop();
+        break;
+     case 3:
+        display();
+        break;
+     case 4:
+        exit(1);
+     default:
+        printf("invalid input\n");
+     }       
+   }
+}
+
 void push(int num)
 {
  struct Node *temp=(struct Node*)malloc(sizeof(struct Node));
+
  if(temp==NULL)
  {
   printf("stack overflow\n");
   return;
  }
+ 
  temp->data=num;
  temp->next=top;
  top=temp;
 }
+
 int pop()
 {
   struct Node *temp;
@@ -65,6 +79,7 @@ int pop()
   printf("the deleted number is=%d",num);
   return num;
 }
+
 void display()
 {
   struct Node *p;
@@ -77,7 +92,8 @@ void display()
   p=top;
   while(p!=NULL)
   {
-   printf("%d\n",p->data);
+   printf("%d ",p->data);
    p=p->next;
   }
+  printf("\n");
 }
