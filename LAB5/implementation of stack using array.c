2036c1986b98a500a27cd8d5b7cb660b880cd77e
Hardik <hardikrana276@gmail.com>
2017-11-11 07:50:18 +0530

    Update implementation of stack using array.c

--- LAB5/implementation of stack using array.c
+++ LAB5/implementation of stack using array.c
@@ -1,80 +1,110 @@
+/* HARDIK RANA
+  
+   C PROGRAM TO IMPLEMENT STACK USING ARRAY
+*/
+
 #include<stdio.h>
 #include<stdbool.h>
-int top=-1;
+
+int top=-1;   //top of the stack
+void display(int stack[],int top);
+int size();
+int topelement(int stack[]);
+int pop();
+ void push(int stack[],int n,int x);
+
+
 int main()
 {
  int n,stack[500],i,x,choice,y;
  char c;
  printf("enter size of stack array\n");
  scanf("%d",&n);
- do{
- printf("enter your choice(1)push (2)pop (3)size (4)top (5)display (6)exit\n");
- scanf("%d",&choice);
- switch(choice)
+ 
+ while(1)
  {
-  case 1:
-    printf("enter the element to be inserted\n");
-    scanf("%d",&x);
-    push(stack,n,x);
-    break;
-  case 2:
-    pop();
-    break;
-  case 3:
-    y=size();
-    printf("the size of stack is\n",size());
-    break;
-  case 4:
-    printf("the top element of stack is\n",topelement());
-    break;
-  case 5:
-     display(stack,top);
+   printf("enter your choice(1)push (2)pop (3)size (4)top (5)display (6)exit\n");
+   scanf("%d",&choice);
+   switch(choice)
+   {
+    case 1:
+      printf("enter the element to be inserted\n");
+      scanf("%d",&x);
+      push(stack,n,x);
+      break;
+   
+    case 2:
+      pop();
+      break;
+   
+    case 3:
+      y=size();
+      printf("the size of stack is=%d\n",size());
+      break;
+   
+   case 4:
+     printf("the top element of stack is=%d\n",topelement(stack));
      break;
-  case 6:
-    exit(0);
-    break;
- }printf("do u want to continue(y/n)\n");
- c=getchar();
- scanf("%c",&c);}
- while('y');
- return 0;
+   
+   case 5:
+      display(stack,top);
+       break;
+   
+   case 6:
+     exit(0);
+     break;
+   }
+ }
+  return 0;
 }
+
+//to display the content of stack
 void display(int stack[],int top)
 {
- int i;
- for(i=top;i>=0;i--)
- {
-  printf("%d\t",stack[i]);
- }}
+  int i;
+  for(i=top;i>=0;i--)
+  {
+   printf("%d ",stack[i]);
+  }
+ printf("\n");
+}
 
+//to push the element into the stack
  void push(int stack[],int n,int x)
  {
-  if(top==n-1)
-  {
-    printf("stack is full\n");
-    return;
-  }
-  else
-  {
-    top=top+1;
-    stack[top]=x;
-  }
+   if(top==n-1)
+   {
+     printf("stack is full\n");
+     return;
+   }
+   else
+   {
+     top=top+1;
+     stack[top]=x;
+   }
  }
- int pop(int top)
+
+//to pop the top element
+int pop()
  {
-  if(top==-1)
-  {
-   printf("stack is empty\n");
-   return;
-  }
-  else
-  {
-    top=top-1;
-  }}
-  int size()
+   if(top==-1)
+   {
+    printf("stack is empty\n");
+    return;
+   }
+   else
+   {
+     top=top-1;
+   }
+ }
+
+//to get the size of stack
+int size()
   {
     return top+1;
   }
+
+//to get the top element of stack
   int topelement(int stack[])
   {
     return stack[top];
